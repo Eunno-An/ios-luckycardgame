7741dec6cb69c567554fed3a44c28dd6b7feddec
Eunno-An <12171647@inha.edu>
2023-07-06 21:56:58 +0900

    [Add] CardDealer에 makeCardPool(), splitCard() 함수 추가

--- LuckyCardGame/LuckyCardGame/Model/Attendee.swift
+++ LuckyCardGame/LuckyCardGame/Model/Attendee.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+/*
+ MARK: 카드 객체 인스턴스를 소유하는 참가자 구조체
+ */
+enum AttendeeNum: Int{
+    case three = 3
+    case four
+    case five
+}
+
+struct Attendee{
+    
+}
--- LuckyCardGame/LuckyCardGame/Model/CardDealer.swift
+++ LuckyCardGame/LuckyCardGame/Model/CardDealer.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+/*
+ MARK: 참가자 수 정보가 주어지면, 전체 카드 풀을 만들고 이를 분배하는 카드딜러 구조체
+ 일단 명수가 주어지면, 각각 해야하는 기능들을 만든다.
+ */
+struct CardDealer{
+    
+    
+}
+extension CardDealer{
+    
+    // MARK: 참석자 수를 받으면, 딜러가 카드 풀을 생성해주는 함수입니다.
+    func makeCardPool(attendeeNum: Int) -> [LuckyCard]{
+        var cardPool : [LuckyCard] = []
+        for animal in Animal.allCases{
+            for number in CardNumber.allCases{
+                if(attendeeNum == 3 && number.rawValue == 12){
+                    continue
+                }
+                cardPool.append(LuckyCard(number: number, animal: animal))
+                
+            }
+        }
+        return cardPool
+    }
+    
+    // MARK: 참석자 수와 cardPool을 받으면, 참석자 수만큼 럭키카드 배열과, 마지막 바닥에 놔야하는 카드를 구분해주는 함수입니다.
+    func splitCard(attendeeNum: AttendeeNum, cardPool: [LuckyCard]) -> [[LuckyCard]]{
+        //attendeeNum에 따라서 참가자들이 가질 럭키카드의 배열들과, 바닥에 둘 카드를 나눠줍니다.
+        var luckyCard2dArr = [[LuckyCard]]()
+        var shuffledCardPool = cardPool.shuffled()
+        
+        if attendeeNum == .three{
+            for i in stride(from:0, through: 16, by: 8){
+                luckyCard2dArr.append(Array(cardPool[i..<i+8]))
+            }
+            luckyCard2dArr.append(Array(cardPool[24..<33]))
+        }
+        else if attendeeNum == .four{
+            for i in stride(from:0, through: 21, by: 7){
+                luckyCard2dArr.append(Array(cardPool[i..<i+8]))
+            }
+            luckyCard2dArr.append(Array(cardPool[28..<36]))
+        }
+        else if attendeeNum == .five{
+            for i in stride(from:0, through: 24, by: 6){
+                luckyCard2dArr.append(Array(cardPool[i..<i+8]))
+            }
+            luckyCard2dArr.append(Array(cardPool[30..<36]))
+        }
+        
+        return luckyCard2dArr
+    }
+}
