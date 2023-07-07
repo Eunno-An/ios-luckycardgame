e030b562a389ead7facc229cb41ea4069c87f509
Eunno-An <12171647@inha.edu>
2023-07-07 11:30:01 +0900

    [Fix] SegmentedControllView에서 addTarget을 ViewController에서 하게끔 수정

--- LuckyCardGame/LuckyCardGame/View/SegmentedControlView.swift
+++ LuckyCardGame/LuckyCardGame/View/SegmentedControlView.swift
@@ -13,10 +13,12 @@ import UIKit
  UIView를 상속받았으며, 현재 frame을 기존의 노란색 블록의 fame으로 대체합니다.
  */
 class SegmentedControlView: UIView{
+    private let uiSegmentControlView = UISegmentedControl(items: ["3명", "4명", "5명"])
     init(){
         super.init(frame: CGRect(x: (CGFloat(Constant.horizontalSpacing)),
                                  y : Constant.topSpacing,
                                  width: (CGFloat(Int(Constant.screenWidth)) - CGFloat(Constant.horizontalSpacing * 2)), height: Constant.topRectHeight))
+        self.setUISegmentedControlView()
         self.addSubview(getUISegmentedControlView())
         
     }
@@ -25,21 +27,10 @@ class SegmentedControlView: UIView{
         fatalError("init(coder:) has not been implemented")
     }
     
-    func getUISegmentedControlView() -> UISegmentedControl{
-        let uiSegmentControlView = UISegmentedControl(items: ["3명", "4명", "5명"])
+    func setUISegmentedControlView(){
         uiSegmentControlView.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: uiSegmentControlView.frame.height)//x:0, y:0으로 설정한 이유는 UISegmentedControl이 view를 부모 뷰에 상대적인 위치로 잡기 때문으로 이해했습니다.
-        uiSegmentControlView.selectedSegmentIndex = 0//처음에는 세명을 기본 선택으로 지정합니다.
-        uiSegmentControlView.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
-        return uiSegmentControlView
     }
-    
-    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl){
-        if sender.selectedSegmentIndex == 0{//3명일때
-            print("3명 선택했습니다")
-        }else if sender.selectedSegmentIndex == 1{//4명일때
-            print("4명 선택했습니다")
-        }else if sender.selectedSegmentIndex == 2{//5명일때
-            print("5명 선택했습니다")
-        }
+    func getUISegmentedControlView() -> UISegmentedControl{
+        return uiSegmentControlView
     }
 }
--- LuckyCardGame/LuckyCardGame/ViewController.swift
+++ LuckyCardGame/LuckyCardGame/ViewController.swift
@@ -11,7 +11,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(SegmentedControlView())
+        let segmentedControlView = SegmentedControlView()
+        let segmentedControl = segmentedControlView.getUISegmentedControlView()
+        self.view.addSubview(segmentedControlView)
+        segmentedControl.selectedSegmentIndex =  0//처음에는 세명을 기본 선택으로 지정합니다.
+        segmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+        
         self.view.addSubview(GameView())
+    }
+    
+    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl){
+        switch sender.selectedSegmentIndex{
+        case 0:
+            print("3명 선택했습니다")
+        case 1:
+            print("4명 선택했습니다")
+        case 2:
+            print("5명 선택했습니다")
+        default:
+            print("OOps")
+        }
+        
     }
 }
