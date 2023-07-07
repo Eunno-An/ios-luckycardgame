0972be7c11557746fccc81b613cc604b7bc4ac67
Eunno-An <12171647@inha.edu>
2023-07-07 10:30:26 +0900

    [Add] SegmentedControllView의 segment 변경에 따른 action listener 추가

--- LuckyCardGame/LuckyCardGame/View/SegmentedControlView.swift
+++ LuckyCardGame/LuckyCardGame/View/SegmentedControlView.swift
@@ -29,6 +29,17 @@ class SegmentedControlView: UIView{
         let uiSegmentControlView = UISegmentedControl(items: ["3명", "4명", "5명"])
         uiSegmentControlView.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: uiSegmentControlView.frame.height)//x:0, y:0으로 설정한 이유는 UISegmentedControl이 view를 부모 뷰에 상대적인 위치로 잡기 때문으로 이해했습니다.
         uiSegmentControlView.selectedSegmentIndex = 0//처음에는 세명을 기본 선택으로 지정합니다.
+        uiSegmentControlView.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
         return uiSegmentControlView
+    }
+    
+    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl){
+        if sender.selectedSegmentIndex == 0{//3명일때
+            print("3명 선택했습니다")
+        }else if sender.selectedSegmentIndex == 1{//4명일때
+            print("4명 선택했습니다")
+        }else if sender.selectedSegmentIndex == 2{//5명일때
+            print("5명 선택했습니다")
+        }
     }
 }
