943ff2f084e8c0def17c8a6cbf0337a92f1b5d94
Eunno-An <12171647@inha.edu>
2023-07-13 01:33:55 +0900

    [Fix] TempSavedCardInfo 구조체에 card의 숫자와 동물을 갖고 있도록 property 추가

--- LuckyCardGame/LuckyCardGame/Model/Mock/Model/TestingPlayerRule/TempSavedCardInfo.swift
+++ LuckyCardGame/LuckyCardGame/Model/Mock/Model/TestingPlayerRule/TempSavedCardInfo.swift
@@ -9,14 +9,18 @@ import Foundation
 
 struct TempSavedCardInfo{
     
-    private(set) var cardIdx: Int // 카드 번호
+    private(set) var cardIdx: Int // 덱에서의 카드 인덱스
     private(set) var isBottomCardOrPlayerCard: Bool // 바닥에서 온 카드인지, 플레이어에서 온 카드인지
     private(set) var playerIdx: Int // 플레이어의 인덱스
+    private(set) var cardNum: CardNumber // 카드 번호
+    private(set) var cardSymbol: Animal // 카드 동물
     
-    init(cardIdx: Int, isBottomCardOrPlayerCard: Bool, playerIdx: Int){
+    init(cardIdx: Int, isBottomCardOrPlayerCard: Bool, playerIdx: Int, cardNum: CardNumber, cardSymbol: Animal){
         self.cardIdx = cardIdx
         self.isBottomCardOrPlayerCard = isBottomCardOrPlayerCard
         self.playerIdx = playerIdx
+        self.cardNum = cardNum
+        self.cardSymbol = cardSymbol
     }
     
 }
